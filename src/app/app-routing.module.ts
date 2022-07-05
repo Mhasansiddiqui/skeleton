b3b42601e3605d16f2da799f33d0cc4662a5b62d
hasan siddiqui <hassan.hashim@tih.org.pk>
2022-07-05 10:31:10 +0500

    code optimized

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { UnisexComponent } from './anatomy/unisex/unisex.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: UnisexComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
