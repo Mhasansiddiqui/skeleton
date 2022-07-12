1ee026199353f464dbbba69d3b0f39b1eb8ad1dd
Mhasansiddiqui <hasancomsoft@gmail.com>
2022-07-12 12:36:05 +0500

    added initail image value

--- src/app/anatomy/unisex/unisex.component.ts
+++ src/app/anatomy/unisex/unisex.component.ts
@@ -1,18 +1,24 @@
-import { Component, Output } from '@angular/core';
+import { Component, Output, OnInit } from '@angular/core';
 import { AnatomyService } from 'src/app/services/anatomy.service';
+import { APPCONSTANT} from 'src/app/constants/anatomy-constnat';
+
 @Component({
   selector: 'app-unisex',
   templateUrl: './unisex.component.html',
   styleUrls: ['./unisex.component.scss']
 })
-export class UnisexComponent {
+export class UnisexComponent implements OnInit {
 
   uniSexConfig = this.anatomyService.uniSexConfig;
   id:number;
+  public appConstant = APPCONSTANT;
 
    constructor(private anatomyService:AnatomyService){
   }
 
+  ngOnInit(){
+    this.changeAnatomy(this.appConstant.ANATOMY.UNISEX);
+  }
    changeAnatomy(id?:number){
        this.id  = id;
    }
