543bad43623dcc7ab677a2b7684a44e395609c6a
hasan siddiqui <hassan.hashim@tih.org.pk>
2022-07-14 10:57:58 +0500

    convert jquery function to angular

--- src/app/components/dynamic-anatomy/dynamic-anatomy.component.ts
+++ src/app/components/dynamic-anatomy/dynamic-anatomy.component.ts
@@ -1,36 +1,36 @@
-import { Component, HostListener, Input } from '@angular/core';
+import { Component, ElementRef, HostListener, Input, ViewChild } from '@angular/core';
 import { iSkeleton } from '@interface/skeleton-interface';
-import { APPCONSTANT} from '@constants/anatomy-constant';
-declare let $: any;
+import { APPCONSTANT } from '@constants/anatomy-constant';
 
 @Component({
   selector: 'app-dynamic-anatomy',
   templateUrl: './dynamic-anatomy.component.html',
   styleUrls: ['./dynamic-anatomy.component.scss']
 })
-export class DynamicAnatomyComponent  {
-  
-  @Input() config : iSkeleton;
-  @Input() id : number;
-  
+export class DynamicAnatomyComponent {
+
+  @Input() config: iSkeleton;
+  @Input() id: number;
+
   public appConstant = APPCONSTANT;
-  
+  @ViewChild('organsTip') organsTip: ElementRef;
+  @ViewChild('frtBase') frtBase: ElementRef;
+
   /**
-   * this code will popup body part discription
+   * this code will print popup body part discription
    * @param event get mouse event
    */
   @HostListener('document:mousemove', ['$event'])
   onMouseMove(event: MouseEvent) {
     var x = event.pageX + 10,
       y = event.pageY + 15;
-    var tipw = $('#organs-tip').outerWidth(),
-      tiph = $('#organs-tip').outerHeight(),
-      x = (x + tipw > $(document).scrollLeft() + $(window).width()) ? x - tipw - (20 * 2) : x
-    y = (y + tiph > $(document).scrollTop() + $(window).height()) ? $(document).scrollTop() + $(window).height() - tiph - 10 : y
-    $('#organs-tip').css({
-      left: x,
-      top: y
-    })
+    var tipw = this.organsTip.nativeElement.style.clientWidth,
+      tiph = this.organsTip.nativeElement.style.clientHeight,
+      x = (x + tipw > document.scrollingElement.scrollLeft + window.innerWidth) ? x - tipw - (20 * 2) : x
+    y = (y + tiph > document.scrollingElement.scrollTop + window.innerHeight) ? $(document).scrollTop() + window.innerHeight - tiph - 10 : y
+
+    this.organsTip.nativeElement.style.left = x + 'px';
+    this.organsTip.nativeElement.style.top = y + 'px';
   }
 
   /**
@@ -39,50 +39,51 @@ export class DynamicAnatomyComponent  {
    */
   @HostListener('click', ['$event.target'])
   onClick(event: any) {
-    if(event.id == 'turn_front'){
-      $('#bck_base').hide().animate({
-        'opacity': '0'
-      }, 300);
-      $('#frt_base').show().animate({
-        'opacity': '1'
-      }, 300)
-    } else if(event.id == 'turn_back'){
-      $('#frt_base').hide().animate({
-        'opacity': '0'
-      }, 300);
-      $('#bck_base').show().animate({
-        'opacity': '1'
-      }, 300)
+    if (event.id) {
+      console.log(this.config[event.id]);
     }
-    
+    if (event.id == 'turn_front') {
+      const bacBase = document.getElementById('bck_base');
+      const frtBase = document.getElementById('frt_base');
+      bacBase.style.display = 'none';
+      frtBase.style.display = 'block';
+    } else if (event.id == 'turn_back') {
+      const bacBase = document.getElementById('bck_base');
+      const frtBase = document.getElementById('frt_base');
+      bacBase.style.display = 'block';
+      frtBase.style.display = 'none';
+
+    }
+
   }
 
-  // on mousemove colour will highlight
+  // on mousemove popup colour will highlight
   @HostListener('mousemove', ['$event.target'])
   onMousemove(event: any) {
     if (event.id && event.id !== 'turn_back' && event.id !== 'turn_front') {
-      var _obj = $('#' + event.id);
-      $('#organs-tip').show().html(this.config[event.id]?.hover);
-      _obj.css({
-        'fill': this.config[event.id]?.overColor,
-        'fill-opacity': this.config[event.id]?.overOpacity,
-        'stroke': this.config[event.id]?.outlineOverColor,
-        'stroke-opacity': this.config[event.id]?.outlineOverOpacity
-      })
+
+      this.organsTip.nativeElement.style.display = 'inline';
+      this.organsTip.nativeElement.innerHTML = this.config[event.id]?.hover;
+
+      const currentObj = document.getElementById(event.id);
+      currentObj.style.fill = this.config[event.id]?.overColor;
+      currentObj.style.fillOpacity = this.config[event.id]?.overOpacity;
+      currentObj.style.stroke = this.config[event.id]?.outlineOverColor;
+      currentObj.style.strokeOpacity = this.config[event.id]?.outlineOverOpacity;
+
     }
   }
 
-  // on mouseout of body re-colour will fire
+  // on mouseout of body popup re-colour will fire
   @HostListener('mouseout', ['$event.target'])
   onMouseout(event: any) {
     if (event.id) {
-      $('#organs-tip').hide();
-      $('#' + event.id).css({
-        'fill': this.config[event.id]?.upColor,
-        'fill-opacity': this.config[event.id]?.upOpacity,
-        'stroke': this.config[event.id]?.outlineUpColor,
-        'stroke-opacity': this.config[event.id]?.outlineUpOpacity
-      })
+      this.organsTip.nativeElement.style.display = 'none';
+      const currentObj = document.getElementById(event.id);
+      currentObj.style.fill = this.config[event.id]?.upColor;
+      currentObj.style.fillOpacity = this.config[event.id]?.upOpacity;
+      currentObj.style.stroke = this.config[event.id]?.outlineUpColor;
+      currentObj.style.strokeOpacity = this.config[event.id]?.outlineUpOpacity;
     }
   }
 
