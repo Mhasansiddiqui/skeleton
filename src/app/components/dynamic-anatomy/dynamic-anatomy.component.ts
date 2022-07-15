d5d0fc9bc4f6d1d4a30476b932ccb01ce5968465
hasan siddiqui <hassan.hashim@tih.org.pk>
2022-07-15 11:06:59 +0500

    refactor code

--- src/app/components/dynamic-anatomy/dynamic-anatomy.component.ts
+++ src/app/components/dynamic-anatomy/dynamic-anatomy.component.ts
@@ -12,9 +12,8 @@ export class DynamicAnatomyComponent {
   @Input() config: iSkeleton;
   @Input() id: number;
 
-  public appConstant : any = APPCONSTANT;
+  public appConstant: any = APPCONSTANT;
   @ViewChild('organsTip') organsTip: ElementRef;
-  @ViewChild('frtBase') frtBase: ElementRef;
 
   /**
    * this code will print popup body part discription
@@ -27,7 +26,7 @@ export class DynamicAnatomyComponent {
     var tipw = this.organsTip.nativeElement.style.clientWidth,
       tiph = this.organsTip.nativeElement.style.clientHeight,
       x = (x + tipw > document.scrollingElement.scrollLeft + window.innerWidth) ? x - tipw - (20 * 2) : x
-    y = (y + tiph > document.scrollingElement.scrollTop + window.innerHeight) ?  document.scrollingElement.scrollTop + window.innerHeight - tiph - 10 : y
+    y = (y + tiph > document.scrollingElement.scrollTop + window.innerHeight) ? document.scrollingElement.scrollTop + window.innerHeight - tiph - 10 : y
 
     this.organsTip.nativeElement.style.left = x + 'px';
     this.organsTip.nativeElement.style.top = y + 'px';
@@ -39,8 +38,9 @@ export class DynamicAnatomyComponent {
    */
   @HostListener('click', ['$event.target'])
   onClick(event: any) {
-    if (event.id) {
-      console.log(this.config[event.id]);
+    if (event.id && this.config[event.id]?.enable) {
+      this.highlightOrgan(event);
+      this.config[event.id].enable = false;
     }
     if (event.id == 'turn_front') {
       const bacBase = document.getElementById('bck_base');
@@ -52,39 +52,68 @@ export class DynamicAnatomyComponent {
       const frtBase = document.getElementById('frt_base');
       bacBase.style.display = 'block';
       frtBase.style.display = 'none';
-
     }
-
   }
 
   // on mousemove popup colour will highlight
   @HostListener('mousemove', ['$event.target'])
   onMousemove(event: any) {
-    if (event.id && event.id !== 'turn_back' && event.id !== 'turn_front') {
+    this.highlightOrgan(event);
+  }
 
-      this.organsTip.nativeElement.style.display = 'inline';
-      this.organsTip.nativeElement.innerHTML = this.config[event.id]?.hover;
-
-      const currentObj = document.getElementById(event.id);
-      currentObj.style.fill = this.config[event.id]?.overColor;
-      currentObj.style.fillOpacity = this.config[event.id]?.overOpacity;
-      currentObj.style.stroke = this.config[event.id]?.outlineOverColor;
-      currentObj.style.strokeOpacity = this.config[event.id]?.outlineOverOpacity;
-
-    }
+  // on mouseout of body popup reset colour will fire
+  @HostListener('mouseout', ['$event.target'])
+  onMouseout(event: any) {
+    this.resetHiglightOrgan(event);
   }
 
   // on mouseout of body popup re-colour will fire
-  @HostListener('mouseout', ['$event.target'])
-  onMouseout(event: any) {
-    if (event.id) {
+  @HostListener('dblclick', ['$event.target'])
+  onMousedblClick(event: any) {
+    this.highlightOrgan(event);
+    this.config[event.id].enable = true;
+  }
+
+  /**
+   * this code will highlight organs areas 
+   * @param event is passing current event object
+   */
+  highlightOrgan(event: any) {
+    if (event.id && event.id !== 'turn_back' && event.id !== 'turn_front' && this.config[event.id]?.enable) {
+      this.organsTip.nativeElement.style.display = 'inline';
+      this.organsTip.nativeElement.innerHTML = this.config[event.id]?.hover;
+      const currentObj = document.getElementById(event.id);
+      let { overColor, overOpacity, outlineOverColor, outlineOverOpacity } = this.config[event.id];
+      this.changeStyle(currentObj, overColor, overOpacity, outlineOverColor, outlineOverOpacity);
+    }
+  }
+
+  /**
+   * this code will reset highlight organs colour
+   * @param event is passing current event object
+   */
+  resetHiglightOrgan(event: any) {
+    if (event.id && this.config[event.id]?.enable) {
       this.organsTip.nativeElement.style.display = 'none';
       const currentObj = document.getElementById(event.id);
-      currentObj.style.fill = this.config[event.id]?.upColor;
-      currentObj.style.fillOpacity = this.config[event.id]?.upOpacity;
-      currentObj.style.stroke = this.config[event.id]?.outlineUpColor;
-      currentObj.style.strokeOpacity = this.config[event.id]?.outlineUpOpacity;
+      let { upColor, upOpacity, outlineUpColor, outlineUpOpacity } = this.config[event.id];
+      this.changeStyle(currentObj, upColor, upOpacity, outlineUpColor, outlineUpOpacity);
     }
+  }
+
+  /**
+   *  this code will update colours of object
+   * @param currentObj is current object
+   * @param fill will set colour
+   * @param fillOpacity will set opacity 
+   * @param stroke will set stroke
+   * @param strokeOpacity will set strock opacity
+   */
+  changeStyle(currentObj: any, fill: string, fillOpacity: string, stroke: string, strokeOpacity: string) {
+    currentObj.style.fill = fill;
+    currentObj.style.fillOpacity = fillOpacity;
+    currentObj.style.stroke = stroke;
+    currentObj.style.strokeOpacity = strokeOpacity;
   }
 
   constructor() { }
